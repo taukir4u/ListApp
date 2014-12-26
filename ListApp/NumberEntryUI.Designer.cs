26ef7fb3285297277568c855c302449cc4069079
taukir4u <taukir4u@gmail.com>
2014-12-26 20:14:20 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ ListApp/NumberEntryUI.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ListApp
+{
+    partial class NumberEntryUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.numberListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sumTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.countTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(260, 57);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(260, 97);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(75, 23);
+            this.showAllButton.TabIndex = 1;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Number";
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(111, 26);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(224, 20);
+            this.numberTextBox.TabIndex = 3;
+            // 
+            // numberListBox
+            // 
+            this.numberListBox.FormattingEnabled = true;
+            this.numberListBox.Location = new System.Drawing.Point(111, 139);
+            this.numberListBox.Name = "numberListBox";
+            this.numberListBox.Size = new System.Drawing.Size(224, 134);
+            this.numberListBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sum Result";
+            // 
+            // sumTextBox
+            // 
+            this.sumTextBox.Location = new System.Drawing.Point(240, 300);
+            this.sumTextBox.Name = "sumTextBox";
+            this.sumTextBox.Size = new System.Drawing.Size(95, 20);
+            this.sumTextBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 305);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Count";
+            // 
+            // countTextBox
+            // 
+            this.countTextBox.Location = new System.Drawing.Point(78, 300);
+            this.countTextBox.Name = "countTextBox";
+            this.countTextBox.Size = new System.Drawing.Size(73, 20);
+            this.countTextBox.TabIndex = 8;
+            // 
+            // NumberEntryUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 325);
+            this.Controls.Add(this.countTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sumTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numberListBox);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.addButton);
+            this.Name = "NumberEntryUI";
+            this.Text = "NumberEntryUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.ListBox numberListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sumTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox countTextBox;
+    }
+}
+
---
+++ ListApp/NumberEntryUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListApp
+{
+    public partial class NumberEntryUI : Form
+    {
+
+        List<double> numbers = new List<double>();
+        public NumberEntryUI()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            double number = Convert.ToDouble(numberTextBox.Text);
+            numbers.Add(number);
+            countTextBox.Text = numbers.Count.ToString();
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            double sum = 0;
+            numberListBox.Items.Clear();
+            foreach (double aNumber in numbers)
+            {
+                
+                numberListBox.Items.Add(aNumber);
+                sum += aNumber;
+                
+            }
+            sumTextBox.Text = sum.ToString();
+        }
+    }
+}
